0c31004ddd75083d40bb4b5d5a0edc7b1cc4e572
py0trandreev <79420905+py0trandreev@users.noreply.github.com>
2021-12-27 22:29:49 +0300

    lesson01 (#1)
    * users added
    
    * api created
    
    * lesson01 code style fixed

---
+++ users/models.py
@@ -0,0 +1,13 @@
+from uuid import uuid4
+
+from django.db import models
+
+
+# Create your models here.
+class User(models.Model):
+    # uuid = models.UUIDField(primary_key=True, default=uuid4())
+    username = models.CharField(max_length=64)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField(unique=True, blank=False)
+    birthday = models.DateField(blank=True)
