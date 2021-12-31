080c5f49b5272d27ef4632718b817d7b7a57c03b
petr <py0trandreev@yandex.ru>
2021-12-31 22:13:32 +0300

    users added

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+   constructor(props) {
+       super(props)
+       this.state = {
+           'authors': []
+       }
+   }
+
+   render () {
+       return (
+           <div>
+               Main App hahaha
+           </div>
+       )
+   }
+}
+
+export default App;
--- users/models.py
+++ users/models.py
@@ -5,9 +5,9 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    # uuid = models.UUIDField(primary_key=True, default=uuid4())
+    uuid = models.UUIDField(primary_key=True, default=uuid4())
     username = models.CharField(max_length=64)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     email = models.EmailField(unique=True, blank=False)
-    birthday = models.DateField(blank=True)
+    birthyear = models.PositiveIntegerField(default=0)
