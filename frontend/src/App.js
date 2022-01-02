c5a6e411fe53d11aea44e0a9f44263b5451ad79d
petr <py0trandreev@yandex.ru>
2022-01-02 18:21:33 +0300

    axios, get user list from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,36 @@
 import React from 'react';
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
+import UserList from './components/User.js'
+import axios from 'axios'
 
 class App extends React.Component {
    constructor(props) {
        super(props)
        this.state = {
-           'authors': []
+           'users': []
        }
+   }
+
+   componentDidMount(){
+     axios.get('http://127.0.0.1:8000/api/users')
+    .then(response => {
+        const users = response.data
+            this.setState(
+            {
+                'users': users
+            }
+        )
+        console.log("response" + response)
+    })
+    .catch(error => console.log(error))
    }
 
    render () {
        return (
-           <div>
-               Main App hahaha
-           </div>
+        <div>
+          <UserList users={this.state.users} />
+        </div>
        )
    }
 }
