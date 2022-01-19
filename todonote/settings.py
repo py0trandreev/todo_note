8346834adeb07cd099235b0e962202a3c304a2a9
petr <py0trandreev@yandex.ru>
2022-01-19 19:50:32 +0300

    lesson04 stub

--- todonote/settings.py
+++ todonote/settings.py
@@ -137,3 +137,9 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 # CORS_ORIGIN_ALLOW_ALL = True
+
+
+REST_FRAMEWORK = {
+   'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+   'PAGE_SIZE': 100
+}
