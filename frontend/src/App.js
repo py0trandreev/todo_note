0391612fdeadba8b2331981e68acf550393b0d37
petr <py0trandreev@yandex.ru>
2022-01-31 16:45:27 +0300

    lesson07 atempt button Login to be changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,18 +13,26 @@ import {ProjectList, ProjectDetail} from './components/Project.js'
 import TodoList from './components/TODO.js'
 import FooterItem from './components/Footer.js'
 import axios from 'axios'
-
+import LoginForm from './components/Auth.js'
+import Cookies from 'universal-cookie';
 
 const DOMAIN = 'http://127.0.0.1:8000'
 const getUrl = (endPoint) => `${DOMAIN}${endPoint}`
 
+const NotFound404 = ({ location }) => {
+  return (
+    <div>
+        <h1>Страница по адресу '{location.pathname}' не найдена</h1>
+    </div>
+  )
+}
 
 class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             navbarItems: [
-                {name: 'Users', href: '/users'},
+                {name: 'Users', href: '/'},
                 {name: 'Projects', href: '/projects'},
                 {name: 'TODOs', href: '/todos'},
             ],
@@ -32,50 +40,80 @@ class App extends React.Component {
             projects: [],
             project: {},
             todos: [],
-
+            'token': '',
         }
-
-
     }
 
+
+    set_token(token) {
+        const cookies = new Cookies();
+        cookies.set('token', token);
+        this.setState({'token': token});
+    }
+
+    is_authenticated() {
+        return this.state.token !== '';
+    }
+
+    logout() {
+        this.set_token('');
+    }
+
+    get_token_from_storage() {
+        const cookies = new Cookies();
+        const token = cookies.get('token');
+        this.setState({'token': token});
+    }
+
+    get_token(username, password) {
+        axios.post(getUrl('/api-token-auth/'), {username: username, password: password})
+            .then(response => {
+                //console.log(response.data)
+                this.set_token(response.data['token'])
+            }).catch(error => alert('Неверный логин или пароль'));
+    }
+
+   load_data() {
+        axios.get(getUrl('/api/users/'))
+            .then(response => {
+                console.log(`response.data ******* ${response.data}`);
+                this.setState({users: response.data.results});
+            }).catch(error => console.log(error));
+
+        axios.get(getUrl('/api/projects/'))
+            .then(response => {
+                this.setState({projects: response.data.results})
+            }).catch(error => console.log(error));
+
+        axios.get(getUrl('/api/todos/'))
+            .then(response => {
+                this.setState({todos: response.data.results})
+            }).catch(error => console.log(error));
+    }
 
     getProject(id) {
         axios.get(getUrl(`/api/projects/${id}`))
         .then(response => {
             this.setState({project: response.data})
-        }).catch(error => console.log(error))
+        }).catch(error => console.log(error));
     }
 
-
     componentDidMount(){
-        axios.get(getUrl('/api/users/'))
-        .then(response => {
-            console.log(`response.data ******* ${response.data}`)
-            this.setState({users: response.data.results})
-        }).catch(error => console.log(error));
-
-        axios.get(getUrl('/api/projects/'))
-        .then(response => {
-            //console.log(response.data)
-            this.setState({projects: response.data.results})
-        }).catch(error => console.log(error));
-
-        axios.get(getUrl('/api/todos/'))
-        .then(response => {
-            this.setState({todos: response.data.results})
-        }).catch(error => console.log(error));
+        this.get_token_from_storage();
+        this.load_data();
    }
 
    render () {
+       console.log("****************" + this.is_authenticated())
        return (
            <Router>
                <header>
-                    <Navbar navbarItems={this.state.navbarItems} />
+                    <Navbar navbarItems={this.state.navbarItems} isLogedIn={this.is_authenticated()} logOut={this.logout}/>
                </header>
-               <main role="main" class="flex-shrink-0">
+               <main role="main" className="flex-shrink-0">
                       <div className="container">
                         <Switch>
-                            <Route exact path='/users'>
+                            <Route exact path={["/", "/users"]}>
                                 <UserList users={this.state.users} />
                             </Route>
                             <Route exact path='/projects'>
@@ -84,6 +122,13 @@ class App extends React.Component {
                             <Route exact path='/todos'>
                                 <TodoList items={this.state.todos} />
                             </Route>
+                            <Route exact path='/login'
+                                   component={
+                                       () => <LoginForm get_token={
+                                                (username, password) => this.get_token(username, password)
+                                            }/>
+                                   }
+                            />
                             <Route path="/project/:id"
                                    children={
                                 <ProjectDetail
@@ -93,7 +138,9 @@ class App extends React.Component {
                                 /> }
 
                             />
+                            <Route component={NotFound404} />
                         </Switch>
+                        <p></p>
                         <FooterItem></FooterItem>
                       </div>
                   </main>
--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -15,23 +15,43 @@ function filter(){
 function NavbarItem({name, href}) {
     return (
         // <li className="nav-item">
+
         <a className="navbar-item has-text-light">
             <Link className="nav-link" to={href}>  {name}  </Link>
         </a>
+
+        // name !== "Login" ?
+        // :
+        //
+        // <a className="navbar-item has-text-light">
+        //     {/*<Link className="nav-link" to={href}>  {name}  </Link>*/}
+        //     {/*{isAuthenticated ? name = "Logout" : name}*/}
+        //     <div hidden>{isAuthenticated ? name = "Logout" : name = "Login"}</div>
+        //     <div hidden>{console.log("*-*-*-*-" + isAuthenticated)}</div>
+        //     <Link className="nav-link" to={href}>{name}</Link>
+        //
+        // </a>
+          //// {isAuthenticated ? <button onClick={()=>this.logout()}>Logout</button> : <Link to='/login'>Login</Link>}
                     // </li>
     )
 }
 
 
-export default function Navbar({ navbarItems }) {
+export default function Navbar({navbarItems, isLogedIn, logOut}) {
     const [title, setTitle] = useState('');
 
+
+
+    console.log(isLogedIn)
     return (
         <div>
             <nav className="navbar is-light" role="navigation" aria-label="main navigation">
                 <div className="navbar-menu">
                     <div className="navbar-start">
-                        { navbarItems.map((item) => <NavbarItem name={ item.name } href={ item.href }/>) }
+                        {navbarItems.map((item) => <NavbarItem name={item.name} href={item.href}/>)}
+                        <Link to='/login'>Login</Link>
+                        <button onClick={()=>this.logout()}>Logout</button>
+
                     </div>
 
                         <div className="navbar-end">
