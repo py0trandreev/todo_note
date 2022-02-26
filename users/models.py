b3a70e1b471b27555f62ee4725ff6b25727c6e79
petr <py0trandreev@yandex.ru>
2022-02-26 23:33:28 +0300

    lesson11a edited

--- users/models.py
+++ users/models.py
@@ -1,11 +1,11 @@
+from django.db import models
 from uuid import uuid4
 
-from django.db import models
 
 
 # Create your models here.
 class User(models.Model):
-    uuid = models.UUIDField(primary_key=True, default=uuid4())
+    uuid = models.UUIDField(primary_key=True, default=uuid4)
     username = models.CharField(max_length=64)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
