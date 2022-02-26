24a9c08f77725e1d1492ce6e3c05c6003d51515d
py0trandreev <79420905+py0trandreev@users.noreply.github.com>
2022-02-26 23:37:41 +0300

    Lesson09 (#11)
    * UrlPathVersioning is working
    
    * Lesson09. Documentation drf-yasg completed
    
    * lesson10 graphQL (#12)
    
    * lesson10 graphQL
    
    * Lesson11 (#13)
    
    * lesson11 delete pros comleted, creating started
    
    * lesson11 project + todo CRD completed
    
    * lesson11 style corrected
    
    * lesson11 comleted
    
    * lesson11a edited

---
+++ frontend/src/components/TodoForm.js
@@ -0,0 +1,79 @@
+import React from 'react'
+
+
+class TodoForm extends React.Component {
+    constructor(props) {
+      super(props)
+
+      this.state = {
+          project: 0,
+          text: '',
+          user:0,
+      }
+    }
+
+    handleChange(event)
+    {
+        this.setState(
+                {
+                    [event.target.name]: event.target.value
+
+                }
+            );
+    }
+
+
+    handleSubmit(event) {
+      console.log(this.state.project)
+      console.log(this.state.text)
+      console.log(this.state.user)
+
+      this.props.createTodo(this.state.project, this.state.text, this.state.user)
+      event.preventDefault()
+    }
+
+    render() {
+        return (
+            <form onSubmit={ (event) => this.handleSubmit(event) }>
+                <div className="field">
+                    <div className="form-group">
+                        <label for="project">project</label>
+                        <div className="control">
+                            <select name="project" className='form-control input is-info'
+                                    onChange={ (event) => this.handleChange(event) }>
+                                { this.props.projects.map((item) => <option value={ item.id }>{ item.name }</option>) }
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="field">
+                    <div className="form-group">
+                        <label for="text">text</label>
+                        <div className="control">
+                            <input type="text" className="form-control input is-info" name="text" value={ this.state.text }
+                                   onChange={ (event) => this.handleChange(event) }/>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="field">
+                    <div className="form-group">
+                        <label For="user">user</label>
+                        <div className="control">
+                            <select name="user" className='form-control input is-info'
+                                    onChange={ (event) => this.handleChange(event) }>
+                                { this.props.users.map((item) => <option
+                                    value={ item.uuid }>{ item.username }</option>) }
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <input type="submit" className="button is-success" value="Save"/>
+            </form>
+        );
+    }
+  }
+
+  export default TodoForm
